5a7e760059c1c31b0867a025c9edba64c541e53e
Andrey Svirin <aaswirin@gmail.com>
2025-12-08 14:47:45 +0300

    Структуры данных

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -2,4 +2,60 @@
  * Фильмы. Структура данных
  */
 
-//TODO описать DTO для запросов к /films
+import {
+  IsMongoId,
+  IsDateString,
+  IsNumber,
+  IsArray,
+  IsFQDN,
+  IsString,
+  IsInt,
+  Min,
+} from 'class-validator';
+
+/**
+ * Расписание
+ */
+export class SheduleDTO {
+  @IsMongoId()
+  id: string; // ID заказа
+  @IsDateString()
+  daytime: string; // Дата заказа
+  @IsInt()
+  @Min(0)
+  hall: number; // Зал
+  @IsInt()
+  @Min(1)
+  rows: number; // Ряд
+  @IsInt()
+  @Min(1)
+  seats: number; // Место
+  @IsNumber({ maxDecimalPlaces: 2 })
+  price: number; // Цена
+  @IsArray()
+  taken: string[]; // Получено??
+}
+
+/* Фильмы */
+export class FilmDTO {
+  @IsMongoId()
+  id: string; // ID заказа
+  @IsString()
+  rating: string; // Рейтинг
+  @IsString()
+  director: string; // Режиссёр
+  @IsArray()
+  tags: string[]; // Теги
+  @IsFQDN()
+  image: string; // Изображение
+  @IsFQDN()
+  cover: string; // Афиша
+  @IsString()
+  title: string; // Название
+  @IsString()
+  about: string; // О фильме
+  @IsString()
+  description: string; // Описание
+  @IsArray()
+  schedule?: SheduleDTO[]; // Расписание
+}
--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -2,4 +2,38 @@
  * Заказ. Структура данных
  */
 
-//TODO реализовать DTO для /orders
+import {
+  IsMongoId,
+  IsInt,
+  Min,
+  IsArray,
+  IsEmail,
+  IsOptional,
+  IsPhoneNumber,
+} from 'class-validator';
+
+/* Билет */
+export class TicketDto {
+  @IsMongoId()
+  film: string; // ID фильма
+  @IsMongoId()
+  session: string; // ID сессии??
+  @IsInt()
+  @Min(1)
+  row: number; // Ряд
+  @IsInt()
+  @Min(1)
+  seat: number; // Место
+}
+
+/* Заказ */
+export class OrderDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string; // Почта
+  @IsOptional()
+  @IsPhoneNumber()
+  phone?: string; // Телефон
+  @IsArray()
+  tickets: TicketDto[]; // Билеты
+}
