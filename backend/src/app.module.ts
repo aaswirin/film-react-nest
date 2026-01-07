11fc455c9c2918a14dc1cc6bb62970d12b81d666
Andrey Svirin <aaswirin@gmail.com>
2026-01-07 17:47:11 +0300

    MongoDB работает

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,31 +3,24 @@
  */
 
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { MongooseModule } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import configuration, { appConfig } from './configuration';
 
-/* Всё про фильмы */
-import { FilmsController } from './films/films.controller';
-import { FilmsService } from './films/films.service';
-import { FilmsRepository } from './repository/films/films.repository';
-import { FilmSchema } from './repository/films/films.types';
-
-/* Всё про заказ */
-import { OrderController } from './order/order.controller';
-import { OrderService } from './order/order.service';
-import { OrderRepository } from './repository/orders/order.repository';
-import { OrderSchema } from './repository/orders/order.types';
+/* Всё про БД */
+import { DatabasesModule } from './repository/databases.module';
+import { OrderModule } from './order/order.module';
+import { FilmsModule } from './films/films.module';
 
 /**
  * Для отладки
  * если DEBUG=*, то показываем запросы к MongoDB
  */
+// TODO отладка в PostgreSQL?
 if (appConfig.DEBUG === '*') {
   mongoose.set('debug', true);
 }
@@ -40,17 +33,9 @@ if (appConfig.DEBUG === '*') {
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('database.url'),
-      }),
-      inject: [ConfigService],
-    }),
-    MongooseModule.forFeature([
-      { name: 'film', schema: FilmSchema },
-      { name: 'orders', schema: OrderSchema },
-    ]),
+    DatabasesModule.forRootAsync(),
+    OrderModule,
+    FilmsModule,
     WinstonModule.forRoot({
       levels: {
         critical_error: 0,
@@ -68,7 +53,7 @@ if (appConfig.DEBUG === '*') {
       ],
     }),
   ],
-  controllers: [FilmsController, OrderController],
-  providers: [FilmsService, FilmsRepository, OrderService, OrderRepository],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/repository/databases.module.ts
+++ backend/src/repository/databases.module.ts
@@ -3,34 +3,65 @@
  */
 
 import { DynamicModule, Global, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import configuration from '../configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import configuration, { appConfig } from '../configuration';
+
+/* Всё про фильмы */
+import { FilmSchema } from './films/films.types';
+import { FilmsRepository } from './films/films.repository';
+import { FilmsService } from '../films/films.service';
+
+/* Всё про заказ */
+import { OrderSchema } from './orders/order.types';
+import { OrderRepository } from './orders/order.repository';
+import { OrderService } from '../order/order.service';
 
 @Global()
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-    }),
-  ],
-})
+@Module({})
 export class DatabasesModule {
   static forRootAsync(): DynamicModule {
-    const driver = process.env.DATABASE_DRIVER || 'postgres';
+    const driver = appConfig.DATABASE_DRIVER;
+    const imports = [
+      ConfigModule.forRoot({
+        load: [configuration],
+      }),
+    ];
+    const providers = [];
 
     switch (driver) {
-      case 'mongo':
+      // MongoDB
+      case 'mongodb':
+        imports.push(
+          MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+              uri: configService.get<string>('database.url'),
+            }),
+            inject: [ConfigService],
+          }),
+          MongooseModule.forFeature([
+            { name: 'film', schema: FilmSchema },
+            { name: 'orders', schema: OrderSchema },
+          ]),
+        );
+
+        providers.push(FilmsService);
+        providers.push(FilmsRepository);
+        providers.push(OrderService);
+        providers.push(OrderRepository);
+
         break;
+      // PostgreSQL
       case 'postgres':
         break;
     }
 
     return {
-      module: null,
-      imports: null,
-      providers: null,
-      exports: null,
+      module: DatabasesModule,
+      imports: imports,
+      providers: providers,
+      exports: providers,
     };
   }
 }
